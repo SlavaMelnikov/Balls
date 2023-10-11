74a3c6b6363845f58df220257747e9961ad58771
Slava Melnikov <simplejkee@gmail.com>
2023-10-11 10:56:20 +0300

    gitignore configuration

--- src/by/melnikov/balls/main/Main.java
+++ src/by/melnikov/balls/main/Main.java
@@ -6,6 +6,7 @@ import by.melnikov.balls.service.BallService;
 import by.melnikov.balls.service.BallServiceImpl;
 
 import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,14 +14,16 @@ public class Main {
 
         List<Ball> balls = ballService.createBalls();
         double totalWeight = ballService.countTotalWeight(balls);
-        int numberOfBallsByColor = ballService.countBallsByColor(balls, Color.BLUE);
 
+        Random random = new Random();
+        int randomColor = random.nextInt(Color.values().length);
+        int numberOfBallsByColor = ballService.countBallsByColor(balls, Color.values()[randomColor]);
 
-        // для проверки
         for (Ball ball : balls) {
             System.out.println(ball);
         }
+        System.out.println();
         System.out.println("Total weight of balls = " + totalWeight);
-        System.out.println("Number of balls of a certain color = " + numberOfBallsByColor);
+        System.out.println("Number of " + Color.values()[randomColor] + " balls = " + numberOfBallsByColor);
     }
 }
